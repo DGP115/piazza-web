5d874c2b1d32ac8594aad9dee73054e66ce3abb0
David Peloso <david.g.peloso@gmail.com>
2026-02-22 19:30:27 -0500

    Add nav menu Strada component and log out button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// DGP add.  Get Strada
+// Prevent duplicate Strada registration in dev reloads
+if (!window.__piazzaStradaInitialized) {
+  window.__piazzaStradaInitialized = true
+  await import("@hotwired/strada")
+}
---
+++ app/javascript/controllers/bridge/nav_menu_controller.js
@@ -0,0 +1,42 @@
+import { BridgeComponent, BridgeElement } from "@hotwired/strada"
+
+// Connects to data-controller="bridge--nav-menu"
+export default class extends BridgeComponent {
+  static component = "nav-menu"
+  static targets = [ "item" ]
+  
+  connect() {
+    this.element.classList.add("is-hidden")
+
+    // Each constituent item in the navigation menu will be designated as a
+    // Stimulus target. When the controller connects to the DOM, we iterate over all
+    // the items, using the BridgeElement utility class to extract information
+    // from each one, and send its details in a JSON message to the native app. We
+    // also hide the element in the web page using a CSS class.
+
+    const items =
+      this.itemTargets
+        .map(item => new BridgeElement(item))
+        .map((item, index) => ({
+          title: item.title,
+          index
+        }))
+    
+    this.send("connect", { items }, message => {
+      const selectedIndex = message.data.selectedIndex
+      const selectedItem = new BridgeElement(
+        this.itemTargets[selectedIndex]
+      )
+
+      selectedItem.click()
+
+    })
+    
+  }
+
+  // We’re also sending a disconnect message so the app can do any required
+  // cleanup to prevent duplicate menu items.
+  disconnect() {
+    this.send("disconnect")
+  }
+}
